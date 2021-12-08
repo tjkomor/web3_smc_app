6a921260547229ae88a3087ae8c8ccf81ba59f16
tyler komoroske <tjkomor@clemson.edu>
2021-12-08 10:24:50 -0700

    add random number generation

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/T5GgGbbHxxYRhUNmgaV_xZLwB3geFvfo',
+      accounts: ['7ae4857cdd3dba97722374eb554dd1a19ab78a443150deda9d4e61185c4a6e58'],
+    },
+  },
 };
--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,36 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther('0.1'),
+    });
     await waveContract.deployed();
+    console.log('Contract addy:', waveContract.address);
 
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        'Contract balance:',
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    let waveCount = await waveContract.getTotalWaves();
+    /*
+     * Let's try two waves now
+     */
+    const waveTxn = await waveContract.wave('This is wave #1');
+    await waveTxn.wait();
 
-    await waveContract.wave();
+    const waveTxn2 = await waveContract.wave('This is wave #2');
+    await waveTxn2.wait();
 
-    waveCount = await waveContract.getTotalWaves();
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        'Contract balance:',
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    await waveContract.connect(randomPerson).wave();
-
-    waveCount = await waveContract.getTotalWaves();
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async () => {
